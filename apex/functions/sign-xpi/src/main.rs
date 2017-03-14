fcf76caf589d35350a53cd2951694e09781292c2
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-03-14 12:30:01 -0400

    sign-xpi: Fetch a file from S3 and examine it
    We need a git version of rusoto because no release yet includes
    https://github.com/rusoto/rusoto/pull/569.

--- apex/functions/sign-xpi/src/main.rs
+++ apex/functions/sign-xpi/src/main.rs
@@ -1,24 +1,33 @@
+extern crate rusoto;
 extern crate rust_apex;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
+use std::default::Default;
 use std::error::Error;
-use std::collections::BTreeMap;
 use std::fmt::{Display, Formatter, Error as FmtError};
+use std::io::Write;
 
+use rusoto::default_tls_client;
+use rusoto::{DefaultCredentialsProvider, Region};
+use rusoto::s3::{GetObjectError, GetObjectRequest, S3Client};
 use serde_json::{Value, to_value};
 
-#[derive(Debug)]
-struct DummyError;
 
-impl Display for DummyError {
+#[derive(Debug)]
+enum SignXPIError {
+    S3GetObjectError(GetObjectError),
+    S3GetObjectHasNoBody,
+}
+
+impl Display for SignXPIError {
     fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
         write!(f, "{:?}", self)
     }
 }
 
-impl Error for DummyError {
+impl Error for SignXPIError {
     fn description(&self) -> &str {
         "dummy"
     }
@@ -47,6 +56,7 @@ struct S3Path {
 struct S3Event {
     event_time: String,
     event_name: String,  // FIXME: enum
+    aws_region: String,
     s3: S3Path,
 }
 
@@ -69,20 +79,33 @@ struct S3BatchResponse {
 }
 
 fn main() {
-    rust_apex::run::<_, _, DummyError, _>(|input: S3BatchEvent, c: rust_apex::Context| {
+    let provider = DefaultCredentialsProvider::new().unwrap();
+    let client = S3Client::new(default_tls_client().unwrap(), provider, Region::UsWest2);
+    rust_apex::run::<_, _, SignXPIError, _>(|input: S3BatchEvent, c: rust_apex::Context| {
         let responses = input.records.iter().map(|event| {
             let ref filename = event.s3.object.key;
             if !filename.ends_with(".xpi") {
-                return S3EventResponse::SkipFileNotXPI(filename.clone());
+                return Ok(S3EventResponse::SkipFileNotXPI(filename.clone()));
             }
 
             if !event.event_name.starts_with("ObjectCreated") {
-                return S3EventResponse::SkipEventNotObjectCreated(event.event_name.clone());
+                return Ok(S3EventResponse::SkipEventNotObjectCreated(event.event_name.clone()));
             }
 
+            let mut get_object_request = GetObjectRequest {
+                bucket: event.s3.bucket.name.to_owned(),
+                key: filename.to_owned(),
+                response_content_type: Some("application/octet-stream".to_owned()),
+                ..Default::default()
+            };
+            let response = try!(client.get_object(&get_object_request).map_err(SignXPIError::S3GetObjectError));
+            let mut stderr = std::io::stderr();
+            writeln!(&mut stderr, "Got response: {:?}", &response);
+            writeln!(&mut stderr, "Body length: {:?}", try!(response.body.ok_or(SignXPIError::S3GetObjectHasNoBody)).len());
+
             // FIXME: point to some other XPI
-            S3EventResponse::UploadedXPI(event.s3.clone())
+            Ok(S3EventResponse::UploadedXPI(event.s3.clone()))
         }).collect();
-        Ok(S3BatchResponse { responses: responses })
+        Ok(S3BatchResponse { responses: try!(responses) })
     });
 }
