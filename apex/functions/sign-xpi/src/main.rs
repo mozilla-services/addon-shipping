97cf20b0704ce5485ab133b59c8252a41d1fba36
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-03-15 17:33:08 -0400

    sign-xpi: Read XPI zip and dump manifest
    This uses the "standard" Rust library for reading/writing zip files. It
    has some unfortunate properties which meant I had to fight with the
    borrow checker a little bit.

--- apex/functions/sign-xpi/src/main.rs
+++ apex/functions/sign-xpi/src/main.rs
@@ -3,22 +3,26 @@ extern crate rust_apex;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate zip;
 
 use std::default::Default;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Error as FmtError};
+use std::io::Read;
 use std::io::Write;
 
 use rusoto::default_tls_client;
 use rusoto::{DefaultCredentialsProvider, Region};
 use rusoto::s3::{GetObjectError, GetObjectRequest, S3Client};
 use serde_json::{Value, to_value};
-
+use zip::result::ZipError;
 
 #[derive(Debug)]
 enum SignXPIError {
     S3GetObjectError(GetObjectError),
     S3GetObjectHasNoBody,
+    XPINotZipFile(ZipError),
+    XPIManifestNotReadable(ZipError),
 }
 
 impl Display for SignXPIError {
@@ -99,9 +103,26 @@ fn main() {
                 ..Default::default()
             };
             let response = try!(client.get_object(&get_object_request).map_err(SignXPIError::S3GetObjectError));
+            let body = try!(response.body.ok_or(SignXPIError::S3GetObjectHasNoBody));
+
+            let cursor = std::io::Cursor::new(body);
+            let mut zip = try!(zip::ZipArchive::new(cursor.clone()).map_err(SignXPIError::XPINotZipFile));
+            // Ugh, upstream zip library doesn't support reading
+            // "multiple files" at the same time. Since we want to
+            // check for "install.rdf" or "manifest.json", we have to
+            // have two ZipArchives. See https://github.com/mvdnes/zip-rs/issues/14.
+            let mut zip2 = try!(zip::ZipArchive::new(cursor).map_err(SignXPIError::XPINotZipFile));
             let mut stderr = std::io::stderr();
-            writeln!(&mut stderr, "Got response: {:?}", &response);
-            writeln!(&mut stderr, "Body length: {:?}", try!(response.body.ok_or(SignXPIError::S3GetObjectHasNoBody)).len());
+            // Ugh, upstream doesn't have an Iterator implementation
+            for i in 0..zip.len() {
+                writeln!(&mut stderr, "found file: {}", zip.by_index(i).expect("read of file by index failed").name());
+            }
+            let mut manifest = try!(zip.by_name("install.rdf")
+                                    .or_else(|_| zip2.by_name("manifest.json"))
+                                    .map_err(SignXPIError::XPIManifestNotReadable));
+            let mut manifest_contents = String::new();
+            manifest.read_to_string(&mut manifest_contents);
+            writeln!(&mut stderr, "Manifest: {}", manifest_contents);
 
             // FIXME: point to some other XPI
             Ok(S3EventResponse::UploadedXPI(event.s3.clone()))
